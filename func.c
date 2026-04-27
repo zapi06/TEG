6c5add86e4c0897e29a7a5952d47411e68fbbdb5
zapi06 <vinizapi2016@gmail.com>
2026-04-27 17:33:06 -0300

    adicionado a main e o arquivo de funções

---
+++ func.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+    int val;
+    struct Node* next;
+} Node;
+
+typedef struct{
+    Node *head;
+    Node *tail;
+    int size;
+} List;
+
+void init(List *l){
+    l->head = NULL;
+    l->tail = NULL;
+    l->size = 0;
+}
+
+void push_back(List *l, int v){
+    Node *node = malloc(sizeof(Node));
+    node->val = v;
+    node->next = NULL;
+
+    if(l->head == NULL){
+        l->head = node;
+        l->tail = node;
+    }else{
+        l->tail->next = node;
+        l->tail = node;
+    }
+
+    l->size++;
+}
+
+int get_size(List *l){
+    return l->size;
+}
+
+void mostra(List *l){
+    Node *it = l->head;
+    while(it != NULL){
+        printf("%d ", it->val);
+        it = it->next;
+    }
+    printf("\n");
+}
+
+void free_list(List *l){
+    Node *cur = l->head;
+    while(cur){
+        Node *tmp = cur;
+        cur = cur->next;
+        free(tmp);
+    }
+
+    l->size = 0;
+}
+
---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "func.c"
+
+int main(){
+
+    List v;
+
+    init(&v);
+
+    int n; scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        int x; scanf("%d", &x);
+        push_back(&v, x);
+    }
+
+    mostra(&v); 
+}
+
