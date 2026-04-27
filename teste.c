747c73a3f112f49d9fe7287bd50520ee5d49b9cf
zapi06 <vinizapi2016@gmail.com>
2026-04-27 16:39:31 -0300

    teste

---
+++ teste.c
@@ -0,0 +1,20 @@
+// zapi
+
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+
+void solve(){ 
+
+}
+
+signed main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int t = 1;
+    cin >> t;
+    while(t--)
+        solve();
+}
+
